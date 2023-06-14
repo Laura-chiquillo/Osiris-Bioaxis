a35b04bc9fcce2aee928e6cf9f722405129bd4b1
Laura Chiquillo <lchiquillo@unbosque.edu.co>
2023-06-13 19:33:15 -0500

    Se agrego producto y proyecto crud

--- Backend/app/proyecto/models.py
+++ Backend/app/proyecto/models.py
@@ -172,10 +172,196 @@ class listaProducto(models.Model):
     proyectoFormuladoProducto = models.CharField(max_length=50)
     proyectoRSUProducto = models.CharField(max_length=50)
 
+class rolProducto(models.Model):
+    id = models.CharField(max_length=50, primary_key=True)
+    rol = models.CharField(max_length=50)
 
+class cuartilEsperado(models.Model):
+    id = models.CharField(max_length=50, primary_key=True)
+    cuartil = [
+        ("A", "A"),
+        ("B", "B"),
+        ("C", "C"),
+        ("D", "D"),
+        ("Q", "Q"),
+        ("RNT", "RNT"),
+    ]
+    cuartil = models.CharField(max_length=50, choices=cuartil, default='A')
 
+class categoriaMinciencias(models.Model):
+    id = models.CharField(max_length=50, primary_key=True)
+    categoria = [
+        ("A1", "A1"),
+        ("A2", "A2"),
+        ("B", "B"),
+        ("C", "C"),
+    ]
+    categoria = models.CharField(max_length=50, choices=categoria, default='Junior')
+
+class estudiantes(models.Model):
+    nombres = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=50)
+    semestre = models.IntegerField()
+    fechaGrado = models.DateField()
+    codigoGrupo = models.CharField(max_length=50)
+    tipoDocumento = [
+        ("CC", "Cédula de ciudadanía"),
+        ("TI", "Tarjeta de identidad"),
+        ("CE", "Cédula de extranjería"),
+        ("RC", "Registro civil"),
+        ("PA", "Pasaporte"),
+    ]
+    tipoDocumento = models.CharField(max_length=50, choices=tipoDocumento, default='CC')
+    numeroDocumento = models.CharField(max_length=50, primary_key=True)
+
+class estadoProducto(models.Model):
+    id = models.CharField(max_length=50, primary_key=True)
+    estado = [
+        ("resaccion", "resaccion"),
+        ("sometido", "sometido"),
+        ("publicado", "publicado"),
+    ]
+    estado = models.CharField(max_length=50, choices=estado, default='En proceso')
+
+class producto(models.Model):
+    id = models.CharField(max_length=50, primary_key=True)
+    tituloProducto = models.CharField(max_length=50)
+    rolProducto = models.ForeignKey(rolProducto,null=False,blank=False,on_delete=models.CASCADE)
+    investigador = models.ForeignKey(investigador,null=False,blank=False,on_delete=models.CASCADE)
+    listaProducto = models.ForeignKey(listaProducto,null=False,blank=False,on_delete=models.CASCADE)
+    cuartilEsperado = models.ForeignKey(cuartilEsperado,null=False,blank=False,on_delete=models.CASCADE)
+    categoriaMinciencias = models.ForeignKey(categoriaMinciencias,null=False,blank=False,on_delete=models.CASCADE)
+    tipologiaProducto = models.CharField(max_length=50)
+    publicacion = models.CharField(max_length=50)
+    estudiantes = models.ForeignKey(estudiantes,null=False,blank=False,on_delete=models.CASCADE)
+    estadoProdIniSemestre = models.CharField(max_length=50)
+    porcentanjeAvanFinSemestre= models.IntegerField()
+    observaciones = models.CharField(max_length=50)
+    estadoProducto = models.ForeignKey(estadoProducto,null=False,blank=False,on_delete=models.CASCADE)
+    porcentajeComSemestral = models.IntegerField()
+    porcentajeRealMensual = models.IntegerField()
+    fecha = models.DateField()
+    origen = models.CharField(max_length=50)
+    Soporte = models.CharField(max_length=50)
 
 # ----------------------- Proyecto -----------------------
+
+class unidadAcademica(models.Model):
+    id = models.CharField(max_length=50, primary_key=True)
+    nombre = models.CharField(max_length=50)
+
+class entidadPostulo(models.Model):
+    id = models.CharField(max_length=50, primary_key=True)
+    nombreInstitucion = models.CharField(max_length=50)
+    nombreGrupo = models.CharField(max_length=50)
+
+class financiacion(models.Model):
+    id = models.CharField(max_length=50, primary_key=True)
+    valorPropuestoFin = models.CharField(max_length=50)
+    valorEjecutadoFin = models.CharField(max_length=50)
+
+class grupoInvestigacionPro(models.Model):
+    id = models.CharField(max_length=50, primary_key=True)
+    nombre = models.CharField(max_length=50)
+
+class transacciones(models.Model):
+    id = models.CharField(max_length=50, primary_key=True)
+    fecha = models.DateField()
+    acta = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=50)
+
+class origen(models.Model):
+    id = models.CharField(max_length=50, primary_key=True)
+    origen = [
+        ("nacional", "nacional"),
+        ("internacional", "internacional"),
+    ]
+    origen = models.CharField(max_length=50, choices=origen, default='nacional')
+
+class ubicacionProyecto(models.Model):
+    id = models.CharField(max_length=50, primary_key=True)
+    instalacion = models.CharField(max_length=50)
+    municipio = models.CharField(max_length=50)
+    pais = models.CharField(max_length=50)
+    departamento = models.CharField(max_length=50)
+
+class estadoProyecto(models.Model):
+    id = models.CharField(max_length=50, primary_key=True)
+    estado = [
+        ("Propuesta", "Propuesta"),
+        ("Iniciado", "Iniciado"),
+        ("Ejecucion", "Ejecucion"),
+        ("Finalizado", "Finalizado"),
+        ("Detenido", "Detenido"),
+    ]
+    estado = models.CharField(max_length=50, choices=estado, default='En proceso')
+
+class modalidadProyecto(models.Model):
+    id = models.CharField(max_length=50, primary_key=True)
+    modalidad = [
+        ("general", "general"),
+        ("clinical", "clinical"),
+        ("creación", "creación"),
+    ]
+    modalidad = models.CharField(max_length=50, choices=modalidad, default='Convocatoria')
+
+class avanceProyecto(models.Model):
+    id = models.CharField(max_length=50, primary_key=True)
+    reporte = models.CharField(max_length=50)
+    entregablesComprometidos = models.CharField(max_length=50)
+    entregablesReal = models.CharField(max_length=50)
+
+class lineaInvestigacion(models.Model):
+    id = models.CharField(max_length=50, primary_key=True)
+    nombre = models.CharField(max_length=50)
+
+class entregableAdministrativo(models.Model):
+    id = models.CharField(max_length=50, primary_key=True)
+    nombre = models.CharField(max_length=50)
+    titulo = models.CharField(max_length=50)
+    calidad = models.CharField(max_length=50)
+    entregable = models.CharField(max_length=50)
+    pendiente = models.CharField(max_length=50)
+    clasificacion = models.CharField(max_length=50)
+
+class proyecto(models.Model):
+    codigo = models.CharField(max_length=50, primary_key=True)
+    fecha = models.DateField()
+    titulo = models.CharField(max_length=50)
+    investigador = models.ForeignKey(investigador,null=False,blank=False,on_delete=models.CASCADE)
+    unidadAcademica = models.ForeignKey(unidadAcademica,null=False,blank=False,on_delete=models.CASCADE)
+    producto = models.ForeignKey(producto,null=False,blank=False,on_delete=models.CASCADE)
+    coinvestigador = models.CharField(max_length=50)
+    programaCoinvestigador = models.CharField(max_length=50)
+    entidadInstitucion = models.CharField(max_length=50)
+    areaDisciplinares = models.CharField(max_length=50)
+    area = models.CharField(max_length=50)
+    porcentajeEjecucionCorte = models.IntegerField()
+    entidadPostulo = models.ForeignKey(entidadPostulo,null=False,blank=False,on_delete=models.CASCADE)
+    financiacion = models.ForeignKey(financiacion,null=False,blank=False,on_delete=models.CASCADE)
+    grupoInvestigacionPro = models.ForeignKey(grupoInvestigacionPro,null=False,blank=False,on_delete=models.CASCADE)
+    porcentajeEjecucionFinCorte = models.IntegerField()
+    porcentajeAvance = models.IntegerField()
+    soporte = models.CharField(max_length=50)
+    transacciones = models.ForeignKey(transacciones,null=False,blank=False,on_delete=models.CASCADE)
+    origen = models.ForeignKey(origen,null=False,blank=False,on_delete=models.CASCADE)
+    convocatoria = models.CharField(max_length=50)
+    ubicacionProyecto = models.ForeignKey(ubicacionProyecto,null=False,blank=False,on_delete=models.CASCADE)
+    estadoProyecto = models.ForeignKey(estadoProyecto,null=False,blank=False,on_delete=models.CASCADE)
+    modalidadProyecto = models.ForeignKey(modalidadProyecto,null=False,blank=False,on_delete=models.CASCADE)
+    nivelRiesgoEtico = models.CharField(max_length=50)
+    lineaInvestigacion = models.ForeignKey(lineaInvestigacion,null=False,blank=False,on_delete=models.CASCADE)
+    entregableAdministrativo = models.ForeignKey(entregableAdministrativo,null=False,blank=False,on_delete=models.CASCADE)
+
+
+
+
+
+
+
+
+
+
 
 
 
--- Backend/app/proyecto/serializer.py
+++ Backend/app/proyecto/serializer.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
 
-from .models import (grupoinvestigacion, investigador, posgrado, pregrado,
-                     ubicacion)
+from .models import (apropiacion, articulos, avanceProyecto, capitulos,
+                     categoriaMinciencias, consultoria, contenido, contrato,
+                     cuartilEsperado, entidadPostulo, entregableAdministrativo,
+                     estadoProducto, estadoProyecto, estudiantes, eventos,
+                     financiacion, grupoinvestigacion, grupoInvestigacionPro,
+                     industrial, investigador, libros, licencia,
+                     lineaInvestigacion, listaProducto, maestria,
+                     modalidadProyecto, origen, posgrado, pregFinalizadoyCurso,
+                     pregrado, producto, proyecto, reconocimientos,
+                     rolProducto, software, transacciones, ubicacion,
+                     ubicacionProyecto, unidadAcademica)
 
 #------------------------ investigador ------------------------
 
@@ -29,4 +38,194 @@ class posgradoSerializer(serializers.ModelSerializer):
     class Meta:
         model = posgrado
         fields = '__all__'
+
+#------------------------ PRODUCTOS ------------------------
+class eventosSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = eventos
+        fields = '__all__'
+
+class articulosSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = articulos
+        fields = '__all__'
+
+class capitulosSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = capitulos
+        fields = '__all__'
+
+class librosSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = libros
+        fields = '__all__'
+
+class softwareSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = software
+        fields = '__all__'
+
+class industrialSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = industrial
+        fields = '__all__'
+
+class reconocimientosSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = reconocimientos
+        fields = '__all__'
+
+class licenciaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = licencia
+        fields = '__all__'
+
+class apropiacionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = apropiacion
+        fields = '__all__'
+
+class contratoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = contrato
+        fields = '__all__'
+
+class consultoriaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = consultoria
+        fields = '__all__'
+
+class contenidoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = contenido
+        fields = '__all__'
+
+class pregFinalizadoyCursoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = pregFinalizadoyCurso
+        fields = '__all__'
+
+class maestriaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = maestria
+        fields = '__all__'
+
+class listaProductoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = listaProducto
+        fields = '__all__'
+
+class rolProductoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = rolProducto
+        fields = '__all__'
+
+class cuartilEsperadoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = cuartilEsperado
+        fields = '__all__'
+
+class categoriaMincienciasSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = categoriaMinciencias
+        fields = '__all__'
+
+class estudiantesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = estudiantes
+        fields = '__all__'
+
+class estadoProductoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = estadoProducto
+        fields = '__all__'
+
+class productoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = producto
+        fields = '__all__'
+
+#------------------------ PROYECTOS ------------------------
+
+class unidadAcademicaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = unidadAcademica
+        fields = '__all__'
+
+class entidadPostuloSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = entidadPostulo
+        fields = '__all__'
+
+class financiacionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = financiacion
+        fields = '__all__'
+
+class grupoInvestigacionCoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = grupoInvestigacionPro
+        fields = '__all__'
+
+class transaccionesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = transacciones
+        fields = '__all__'
+
+class origenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = origen
+        fields = '__all__'
+
+class ubicacionProyectoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ubicacionProyecto
+        fields = '__all__'
+
+class estadoProyectoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = estadoProyecto
+        fields = '__all__'
+
+class modalidadProyectoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = modalidadProyecto
+        fields = '__all__'
+
+class avanceProyectoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = avanceProyecto
+        fields = '__all__'
+
+class lineaInvestigacionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = lineaInvestigacion
+        fields = '__all__'
+
+class entregableAdministrativoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = entregableAdministrativo
+        fields = '__all__'
+
+class proyectoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = proyecto
+        fields = '__all__'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
