6052c2af131a5ceff872e3a0edd4053325999948
Laura Chiquillo <lchiquillo@unbosque.edu.co>
2023-12-15 11:27:22 -0500

    Administtrador - perfil

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdministradorComponent } from './components/menu/administrador/administrador.component';
 import { ControlComponent } from './components/menu/administrador/control/control.component';
 import { EstadosComponent } from './components/menu/administrador/estados/estados.component';
+import { PerfilAdministradorComponent } from './components/menu/administrador/perfil-administrador/perfil-administrador.component';
 import { ConsultasComponent } from './components/menu/investigadores/consultas/consultas.component';
 import { InvesigadoresComponent } from './components/menu/investigadores/invesigadores.component';
 import { ParticipacionComponent } from './components/menu/investigadores/participacion/participacion.component';
@@ -20,6 +21,7 @@ const routes: Routes = [
   {path:'administrador',component:AdministradorComponent},
   {path:'administrador/control',component:ControlComponent},
   {path:'administrador/estados',component:EstadosComponent},
+  {path:'administrador/perfil',component:PerfilAdministradorComponent}
 ];
 
 @NgModule({
---
+++ Frontend/src/app/components/menu/administrador/perfil-administrador/perfil-administrador.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'app-perfil-administrador',
+  templateUrl: './perfil-administrador.component.html',
+  styleUrls: ['./perfil-administrador.component.css'],
+  standalone: true,
+  imports: [FormsModule, MatFormFieldModule, MatInputModule],
+})
+export class PerfilAdministradorComponent {
+
+  imagenURL: string = 'https://i.pinimg.com/originals/67/32/52/673252bd4db4eff03c2a07e5c4d60692.jpg'; // URL de tu imagen
+
+  // Asignando la URL de la imagen a la variable urlDeLaImagen
+  urlDeLaImagen: string = this.imagenURL;
+
+  
+  // activar y inactivar input
+  
+  inputDeshabilitado: boolean = true;
+
+  activarInput() {
+    this.inputDeshabilitado = false; // Activar el input
+  }
+
+  desactivarInput() {
+    this.inputDeshabilitado = true; // Desactivar el input
+  }
+}
