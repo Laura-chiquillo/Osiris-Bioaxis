910e09724796414d055003ffd4b5bb6d3b326c11
Laura Chiquillo <lchiquillo@unbosque.edu.co>
2024-01-02 17:55:04 -0500

    Cambios en perfil investigador

--- Frontend/src/app/components/menu/investigadores/perfil-investigador/perfil-investigador.component.ts
+++ Frontend/src/app/components/menu/investigadores/perfil-investigador/perfil-investigador.component.ts
@@ -1,13 +1,68 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { InvesigadoresComponent } from '../invesigadores.component';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-perfil-investigador',
   templateUrl: './perfil-investigador.component.html',
   styleUrls: ['./perfil-investigador.component.css'],
   standalone: true,
-  imports: [InvesigadoresComponent],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, CommonModule],
 })
 export class PerfilInvestigadorComponent {
+  
+  // imagen
+  imagenURL: string = 'https://i.pinimg.com/originals/67/32/52/673252bd4db4eff03c2a07e5c4d60692.jpg'; // URL de tu imagen
+  
+  urlDeLaImagen: string = this.imagenURL;
+  
+  //Agregar nuevo input
+  inputs: any[] = [];
+  agregarInput() {
+    this.inputs.push({
+      tipo: '',
+      titulo: '',
+      fecha: '',
+      lugar: ''
+    });
+  }
 
+  inputsP: any[] = [];
+  agregarInputPre() {
+    this.inputsP.push({
+      tipo: '',
+      titulo: '',
+      fecha: '',
+      lugar: ''
+    });
+  }
+  
+  // activar y inactivar input
+  mostrarNuevo: boolean = false
+  mostrarInput: boolean = false;
+  inputDeshabilitado: boolean = true;
+  deshabilitarNuevosInputs: boolean = true;
+  
+  activarInput() {
+    this.inputDeshabilitado = false;
+    this.mostrarInput = true;
+    this.mostrarNuevo = false;
+  }
+
+  desactivarInput() {
+    this.inputDeshabilitado = true;
+    this.mostrarInput = false;
+    this.mostrarNuevo = true;
+    this.deshabilitarNuevosInputs = true;
+  }
+
+  cancelarInput(){
+    this.inputDeshabilitado = true;
+    this.mostrarInput = false;
+    this.mostrarNuevo = true;
+    this.inputs = [];
+    this.inputsP = []; // Reiniciar el array inputsP para eliminar los inputs creados
+  }
 }
