e561ce5c007e9dcfe6e2ad88e2b0607b720aa56b
Yuli Currea <ycurrea@unbosque.edu.co>
2024-09-18 19:08:11 -0500

    notificaciones

--- Frontend/src/app/components/menu/investigadores/invesigadores.component.ts
+++ Frontend/src/app/components/menu/investigadores/invesigadores.component.ts
@@ -17,7 +17,11 @@ import { MatBadgeModule } from '@angular/material/badge';
 import { MatMenuModule } from '@angular/material/menu';
 import { CommonModule } from '@angular/common';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { ChangeDetectorRef } from '@angular/core';  
 
+
+import { Subject , of} from 'rxjs';
+import { takeUntil ,  catchError} from 'rxjs/operators';
 @Component({
   selector: 'app-invesigadores',
   templateUrl: './invesigadores.component.html',
@@ -43,6 +47,9 @@ export class InvesigadoresComponent implements OnInit {
   usuarios: any[] = [];
   notificaciones: any[] = [];
   notificacionesHistorial: any[] = [];
+  private unsubscribe$ = new Subject<void>();
+  private notificacionesLeidasLocalmente: Set<string> = new Set();
+
 
   constructor(
     private searchService: SearchService, 
@@ -50,12 +57,18 @@ export class InvesigadoresComponent implements OnInit {
     private investigadorService: InvestigadorService,
     private dialog: MatDialog,
     private router: Router,
+    private cdr: ChangeDetectorRef
   ) {}
 
   ngOnInit() {
     this.obtenerDatosUsuarioSesion();
     this.obtenerNotificaciones();
   }
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
 
   onSearchInputChange(event: any) {
     this.searchService.setSearchQuery(event.target.value);
@@ -96,25 +109,18 @@ export class InvesigadoresComponent implements OnInit {
      * Filtra las notificaciones activas y las ordena por ID en orden descendente.
      * También filtra todas las notificaciones del usuario para el historial y las ordena.
      */
-    this.investigadorService.getNotifications().subscribe(
-      (data) => {
-        // Filtra las notificaciones activas del usuario y las ordena por ID en orden descendente
-        this.notificaciones = data
-          .filter(x => x.destinatario === this.usuarioSesion.numerodocumento && x.estado)
-          .sort((a, b) => (Number(a.id) > Number(b.id) ? -1 : 1));
-        
-        // Filtra todas las notificaciones del usuario para el historial y las ordena por ID en orden descendente
-        this.notificacionesHistorial = data
-          .filter(x => x.destinatario === this.usuarioSesion.numerodocumento)
-          .sort((a, b) => (Number(a.id) > Number(b.id) ? -1 : 1));
-      },
-      (error) => {
-        // Manejo de errores al obtener las notificaciones
-        console.error('Error al obtener notificaciones:', error);
-      }
-    );
+    this.investigadorService.getNotifications()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(
+        (data) => {
+          this.actualizarNotificaciones(data);
+        },
+        (error) => {
+          console.error('Error al obtener notificaciones:', error);
+        }
+      );
   }
-  
+ 
   openDialogoNotificaciones(): void {
     /**
      * Abre el diálogo de notificaciones.
@@ -138,28 +144,58 @@ export class InvesigadoresComponent implements OnInit {
       }
     });
   }
-  
+  actualizarNotificaciones(data: any[]) {
+    this.notificacionesHistorial = data
+      .filter(x => x.destinatario === this.usuarioSesion.numerodocumento)
+      .sort((a, b) => (a.id > b.id ? -1 : 1));
+    
+    this.notificaciones = this.notificacionesHistorial
+      .filter(x => x.estado && !this.notificacionesLeidasLocalmente.has(x.id));
+    
+    console.log('Notificaciones actualizadas:', this.notificaciones);
+    console.log('Historial de notificaciones actualizado:', this.notificacionesHistorial);
+    
+    this.cdr.detectChanges();
+  }
+
   limpiarNotificacion(notificacion: any) {
-    /**
-     * Marca una notificación como leída.
-     * 
-     * Llama al servicio para marcar la notificación como leída y actualiza las notificaciones del componente.
-     * 
-     * @param notificacion La notificación a marcar como leída.
-     */
-    this.investigadorService.leerNotificacion(notificacion).subscribe(
-      (data) => {
-        // Re-inicializa las notificaciones después de marcar una como leída
-        this.ngOnInit();
-      },
-      (error) => {
-        // Manejo de errores al marcar la notificación como leída
-        console.error('Error al obtener notificaciones:', error);
+    if (!notificacion || !notificacion.id) {
+      console.error('Notificación no válida o ID de notificación no definido');
+      return;
+    }
+
+    console.log('Intentando limpiar notificación:', notificacion);
+
+    this.investigadorService.leerNotificacion({ id: notificacion.id, estado: false })
+    .pipe(
+      takeUntil(this.unsubscribe$),
+      catchError(error => {
+        console.error('Error al marcar la notificación como leída:', error);
+        return of(null);
+      })
+    )
+    .subscribe(
+      (response) => {
+        console.log('Respuesta del servidor al marcar como leída:', response);
+  
+        // Marcar la notificación como leída localmente
+        this.notificacionesLeidasLocalmente.add(notificacion.id);
+  
+        // Actualizar las listas de notificaciones
+        this.notificaciones = this.notificaciones.filter(n => n.id !== notificacion.id);
+        const index = this.notificacionesHistorial.findIndex(n => n.id === notificacion.id);
+        if (index !== -1) {
+          this.notificacionesHistorial[index].estado = false;
+        }
+  
+        // Forzar una actualización de la vista
+        this.cdr.detectChanges();
+  
+        // Volver a cargar todas las notificaciones después de un breve retraso
+        setTimeout(() => this.obtenerNotificaciones(), 1000);
       }
     );
   }
-  
-
   navigateSection(route:string): any {
     this.router.navigate([route]);
   }
